f7c508a07f45792af2b7afb95bc7b0c5a3e154fc
Gustavo Ticora <gaticora@gmail.com>
2022-04-18 11:59:29 -0500

    se configuran las rutas base del proyecto las cuales eprmititra ver editar eliminar un producto.

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('main');
+
+
+Route::get('products', function () {
+    return 'esta es la lista de productos';
+})->name('products.index');
+
+
+Route::get('products/create', function () {
+    return 'esta es el formulario para crear un producto';
+})->name('products.create');
+
+Route::post('products', function () {
+    //return 'esta es el formulario para crear un producto';
+})->name('products.store');
+
+
+Route::get('products/{product}', function ($product) {
+    return "mostrar un producto con el id {$product}";
+})->name('products.show');
+
+
+Route::get('products/{product}/edit', function ($product) {
+    return "formulario para editar un producto con el id {$product}";
+})->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}/edit', function ($product) {
+    // return "formulario para editar un producto con el id {$product}";
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+    // return "formulario para editar un producto con el id {$product}";
+})->name('products.destroy');
+
